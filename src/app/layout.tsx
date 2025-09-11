d161e2cc52e66d308b304c62706e7bfbb78f0cc1
I Made Bintang Astawa <astawabintang@gmail.com>
2025-09-11 15:47:00 +0800

    add meta di head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="x8nsNCwkJrUBwNS3J0X8QJuLKoi7CcLqW3uG76HWhQU" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
