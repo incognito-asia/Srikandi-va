a7035d8e8900477ca45c4d44d56b7886b2bbb8c9
I Made Bintang Astawa <astawabintang@gmail.com>
2025-09-11 16:34:48 +0800

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
@@ -7,9 +6,6 @@ export const metadata: Metadata = {
   title: "Srikandi VA",
   description: "Virtual Assistant Service",
 };
-
-
-
 
 export default function RootLayout({
   children,
@@ -20,13 +16,14 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="google-site-verification" content="x8nsNCwkJrUBwNS3J0X8QJuLKoi7CcLqW3uG76HWhQU" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
       </head>
       <body>
         <Toaster position="top-right" />
-        
+
         {children}
       </body>
     </html>
