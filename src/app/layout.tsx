0f2aa8747ebee7bfbb882a73e43b911cc789c7b2
I Made Bintang Astawa <astawabintang@gmail.com>
2025-09-12 10:21:22 +0800

    add gsc dan google analytic tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 
 export const metadata: Metadata = {
   title: "Srikandi VA",
   description: "Virtual Assistant Service",
+  verification: {
+    google: "x8nsNCwkJrUBwNS3J0X8QJuLKoi7CcLqW3uG76HWhQU",
+  },
 };
 
 export default function RootLayout({
@@ -15,13 +20,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta name="google-site-verification" content="x8nsNCwkJrUBwNS3J0X8QJuLKoi7CcLqW3uG76HWhQU" />
+        
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet" />
       </head>
       <body>
+        <GoogleAnalytics gaId="G-25MEE58LXK" />
         <Toaster position="top-right" />
 
         {children}
